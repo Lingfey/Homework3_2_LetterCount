a212493e3dbfa547da786b7d4cb05428f47e32ba
Lingfey <bokanchalangman@gmail.com>
2022-09-23 01:47:38 +0800

    Letter Count

---
+++ Homework3_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homework3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Dictionary<char, int> letterCount = new Dictionary<char, int>();
+            List<char> nonLetters = new List<char>
+            {
+                '.', '!', ',', '?', ':', ';', '#', '@', '$', '&', '(',')', '-',
+                '+', '=', '\"','\'', ' ', '\n', '1', '2', '3', '4', '5', '6',
+                '7', '8','9', '0'
+            };
+            Console.WriteLine("Enter your text");
+            text = Console.ReadLine();
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!letterCount.ContainsKey(text[i]))
+                {
+                    if (!nonLetters.Contains(text[i]))
+                    {
+                        letterCount.Add(text[i], 1);
+                    }
+                }
+                else
+                {
+                    letterCount[text[i]] += 1;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in letterCount)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
